822c689c1cadd4157425dfebc1f22845b5445ca2
Worome <41673409+Worome@users.noreply.github.com>
2019-08-13 12:54:43 +0200

    Hechos cálculos

--- app/src/main/java/com/trianacodes/script/app_radiobutton/MainActivity.java
+++ app/src/main/java/com/trianacodes/script/app_radiobutton/MainActivity.java
@@ -2,6 +2,7 @@ package com.trianacodes.script.app_radiobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -19,7 +20,35 @@ public class MainActivity extends AppCompatActivity {
 
         et_num1 = findViewById(R.id.et_numero1);
         et_num2 = findViewById(R.id.et_numero2);
-
+        tv_resultado = findViewById(R.id.tv_resultado);
+        rbSum = findViewById(R.id.rb_suma);
+        rbRes = findViewById(R.id.rb_resta);
 
     }
+
+    public void Calculo(View view){
+
+        int valor1 = Integer.parseInt(et_num1.getText().toString());
+        int valor2 = Integer.parseInt(et_num2.getText().toString());
+
+        if (rbSum.isChecked()){
+            Suma(valor1, valor2);
+        } else {
+            Resta(valor1, valor2);
+        }
+
+    }
+
+    public int Suma (int v1, int v2){
+
+        return v1 + v2;
+
+    }
+
+    public int Resta (int v1, int v2){
+
+        return v1 - v2;
+
+    }
+
 }
